d80fa8076313b3d1acc949c31136f8c35fc363a3
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-05-28 14:30:59 +0300

    Add BookList and BookForm container components inside containers directory

---
+++ src/containers/BookForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookForm = () => {
+  return (
+    <div>
+      <h1>Book Form</h1>
+    </div>
+  );
+};
+
+export default BookForm;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BooksList = () => {
+  return (
+    <div>
+      <h1>BooksList</h1>
+    </div>
+  );
+};
+
+export default BooksList;
