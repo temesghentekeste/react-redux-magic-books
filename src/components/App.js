8a929854651fea502e4b03b70d8fec18787b9c6a
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-05-28 14:43:59 +0300

    Render BookForm and BookList inside App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import BookForm from '../containers/BookForm';
+import BooksList from '../containers/BooksList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Magic Books</h1>
+      <BookForm />
+      <BooksList />
     </div>
   );
 }
