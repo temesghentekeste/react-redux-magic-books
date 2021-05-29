fc6fc18ce2014ae7cd36c41f88e0ef92fbd1329e
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-05-29 13:23:48 +0300

    Update BookForm component, add form element

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
 
-const BookForm = () => (
-  <div>
-    <h1>Book Form</h1>
-  </div>
-);
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
+const BookForm = () => {
+  const titleRef = useRef('');
+
+  useEffect(() => {
+    titleRef.current.focus();
+  });
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          name="title"
+          ref="titleRef"
+          placeholder="Book Title"
+        />
+        <select name="category">
+          {categories.map((category, index) => (
+            <option key={index}>{category}</option>
+          ))}
+        </select>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
